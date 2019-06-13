cd806ba7a80f6d26428e590714783d664a460f0a
Jack Gay <jackpowis@gmail.com>
2019-06-13 15:27:35 -0400

    working on list item design

--- perry-pediatrics/src/components/Header/Header.jsx
+++ perry-pediatrics/src/components/Header/Header.jsx
@@ -6,8 +6,24 @@ class Header extends Component {
     render() {
         return (
             <div className="header">
+                <div>
                 <Link to='/'><h1>Perry Pediatrics</h1></Link>
+                </div>
+
+                <nav className="navbar" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">  
+                </div>
+
+                <div className="nav-options">
+            <ul className="header-nav">
                 
+                <li><Link to='/about'>About</Link></li>
+               <li> <Link to='/services'>Services</Link></li>
+                <li><Link to='/visit'>Visit</Link></li>
+                <li><Link to='/contact'>Contact</Link></li>
+                </ul>
+                </div>
+                </nav>
             </div>
         );
     }
--- perry-pediatrics/src/components/Home/Home.jsx
+++ perry-pediatrics/src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                
+                <h1>Welcome to the Thunderdome</h1>
             </div>
         );
     }
