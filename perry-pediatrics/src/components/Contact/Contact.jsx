46786828d90bcbd5d4d74ae0a8fa9018d390f3b1
Jack Gay <jackpowis@gmail.com>
2019-06-13 17:36:52 -0400

    added info in the contact form

--- perry-pediatrics/src/components/Contact/Contact.jsx
+++ perry-pediatrics/src/components/Contact/Contact.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import './Contact.css'
 
 class Contact extends Component {
     render() {
         return (
             <div className="Contact">
-                
+                <h2>Phone Number:</h2>
+                <p>(717) 582-2181</p>
+
             </div>
         );
     }
--- perry-pediatrics/src/components/Visit/Visit.jsx
+++ perry-pediatrics/src/components/Visit/Visit.jsx
@@ -8,9 +8,6 @@ class Visit extends Component {
                 <h2>Address:</h2>
                 <p>106 Centre Dr</p>
                 <p>New Bloomfield, PA 17068</p>
-
-                <h2>Phone Number:</h2>
-                <p>(717) 582-2181</p>
             </div>
         );
     }
