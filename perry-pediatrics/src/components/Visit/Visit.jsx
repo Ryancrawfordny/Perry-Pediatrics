4cee90ff7b36d096810fc95202cb4d879b2e0166
Jack Gay <jackpowis@gmail.com>
2019-06-17 13:21:23 -0400

    implementing map within visit

--- perry-pediatrics/src/components/Visit/Visit.jsx
+++ perry-pediatrics/src/components/Visit/Visit.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Visit.css'
+import Map from '../Map/Map'
 
 class Visit extends Component {
     render() {
@@ -8,6 +9,8 @@ class Visit extends Component {
                 <h2>Address:</h2>
                 <p>106 Centre Dr</p>
                 <p>New Bloomfield, PA 17068</p>
+
+                <Map />
             </div>
         );
     }
