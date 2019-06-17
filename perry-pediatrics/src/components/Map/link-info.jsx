e07b9d3b7bc0ad57172ed425d0cb31b4cca902a5
Jack Gay <jackpowis@gmail.com>
2019-06-17 15:20:28 -0400

    got map to entirely render in the center of the page

---
+++ perry-pediatrics/src/components/Map/link-info.jsx
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+
+class MapInfo extends PureComponent {
+    render() {
+        return (
+            <div>
+                <p>106 Centre Dr</p>
+                <p>New Bloomfield, PA 17068</p>
+            </div>
+        )
+    }
+}
+
+export default MapInfo
--- perry-pediatrics/src/components/Visit/Visit.jsx
+++ perry-pediatrics/src/components/Visit/Visit.jsx
@@ -6,11 +6,14 @@ class Visit extends Component {
     render() {
         return (
             <div className="Visit">
+                <div className="visit-address">
                 <h2>Address:</h2>
                 <p>106 Centre Dr</p>
                 <p>New Bloomfield, PA 17068</p>
-
-                <Map />
+                </div>
+                <div className="mapContainer" isAlign="center">
+                    <Map component={Map} />
+                </div>
             </div>
         );
     }
