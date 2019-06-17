ab9f76880f2bdd0756aa87c0521624c124b525d7
Ryan Crawford <ryancrawfordny@gmail.com>
2019-06-17 14:49:35 -0400

    working on the homepage.

--- perry-pediatrics/src/components/Home/Home.jsx
+++ perry-pediatrics/src/components/Home/Home.jsx
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Home extends Component {
-    render() {
-        return (
-            <div className="Home">
-                <h1></h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+<section class="hero">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Welcome to Perry Pediatrics
+      </h1>
+      <h2 class="subtitle">
+        info will go here
+      </h2>
+    </div>
+  </div>
+</section>
+    );
+  }
 }
 
 export default Home;
