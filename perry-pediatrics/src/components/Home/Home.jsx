c0527a3df1368ef2852d10da2d08fa5c2340961a
Jack Gay <jackpowis@gmail.com>
2019-07-08 17:12:21 -0400

    commit

--- perry-pediatrics/src/components/Home/Home.jsx
+++ perry-pediatrics/src/components/Home/Home.jsx
@@ -11,7 +11,6 @@ class Home extends Component {
           <div className="container">
             <h1 className="title">Welcome to Perry Pediatrics</h1>
             <p>At Perry Pediatrics we strive to provide the best medical care for your child from birth through college!</p>
-            {/* <img src={StockImage} alt="doctors"/> */}
           </div>
         </div>
       </section>
