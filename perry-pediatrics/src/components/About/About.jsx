5515d5501ce006ed90a7ad84a18b01c0250badb2
Ryan Crawford <ryancrawfordny@gmail.com>
2019-07-08 12:23:24 -0400

    adding information to various pages so we have something to work on

--- perry-pediatrics/src/components/About/About.jsx
+++ perry-pediatrics/src/components/About/About.jsx
@@ -4,7 +4,7 @@ class About extends Component {
     render() {
         return (
             <div className="About">
-                <p></p>
+                <h2>Welcome to our practice and thank you for choosing Peery Pediatrics. We look forward to getting to know your family and working together to provide the best posible care for your child.</h2>
             </div>
         );
     }
--- perry-pediatrics/src/components/Home/Home.jsx
+++ perry-pediatrics/src/components/Home/Home.jsx
@@ -10,6 +10,7 @@ class Home extends Component {
         <div class="hero-body">
           <div class="container">
             <h1 class="title">Welcome to Perry Pediatrics</h1>
+            <p>At Perry Pediatrics we strive to provide the best medical care for your child from birth through college!</p>
             <img src={StockImage} alt="doctors"/>
           </div>
         </div>
