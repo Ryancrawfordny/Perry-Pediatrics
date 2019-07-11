2f7a153cf6729dab2691927121107882a619c1aa
Ryan Crawford <ryancrawfordny@gmail.com>
2019-07-11 15:25:48 -0400

    added some basic services to page. Working on formatting and styling

--- perry-pediatrics/src/components/Services/Services.jsx
+++ perry-pediatrics/src/components/Services/Services.jsx
@@ -1,16 +1,28 @@
-import React, { Component } from 'react';
-import './Services.css';
+import React, { Component } from "react";
+import "./Services.css";
 
 class Services extends Component {
-    render() {
-        return (
-            <div className="services">
-                <h1 className="services-heading">
-                Services we provide
-                </h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="services">
+        <h1 className="services-heading">Services We Provide</h1>
+        <div className="services-header">
+          <ul>
+            <li>Visits for expecting parents</li>
+            <li>
+              Visits for new or transfer patients, from newborn to college
+            </li>
+            <li>
+              Comprehensive health maintenance/well care to infants, children
+              and adolescents
+            </li>
+            <li>Routine vaccinations</li>
+            <li>Acute/sick care</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Services;
