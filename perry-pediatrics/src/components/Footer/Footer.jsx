250f4e035dd5005c08726851b125a13142dfaa31
Jack Gay <jackpowis@gmail.com>
2019-07-11 13:31:46 -0400

    commit

--- perry-pediatrics/src/components/Footer/Footer.jsx
+++ perry-pediatrics/src/components/Footer/Footer.jsx
@@ -4,11 +4,11 @@ import "./Footer.css";
 class Footer extends Component {
   render() {
     return (
-      <footer className="footer">                    
+      <footer className="footer body-line-height">                    
         <div className="content-line-height has-text-right">
           <div className="content has-text-centered" />
           <strong>Perry Pediatrics</strong>
-          <ul>
+          <ul className="address">
             <p> 106 Centre Dr </p>
             <p>New Bloomfield, PA 17068</p>
           </ul>
