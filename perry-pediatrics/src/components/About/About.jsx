e8f638fdf8cdb1952d1563052b6cf70398ca9299
Jack Gay <jackpowis@gmail.com>
2019-07-11 18:28:14 -0400

    commit

--- perry-pediatrics/src/components/About/About.jsx
+++ perry-pediatrics/src/components/About/About.jsx
@@ -1,41 +1,75 @@
 import React, { Component } from "react";
 import "./About.css";
+import MapPin from "../Map/map-pin";
 
 
 class About extends Component {
-  render() {
-    return (
-      <div className="about">
-       
-                {/* Welcome to our practice and thank you for choosing Perry
-                Pediatrics. We look forward to eating your family and
-                working together to cook your child.
-              
+    render() {
+        return (
+            <div className="about">
+
+                <div className="tile is-ancestor">
+                    <div className="tile is-4 is-vertical is-parent">
+                        <div className="tile is-child box">
+                             <p className="title">About us...</p><p className="subtitle"> Welcome to our practice and thank you for choosing Perry
+                              Pediatrics. We look forward to eating your family and
+        working together to cook your child.</p>
+                        </div>
+                        <div className="tile is-child box">
+                            <p className="title">Children's Health Insurance Plans (CHIP):</p>
+                            <ul>
+                                <li> Highmark</li>
+                                <li>Aetna Better Health</li>
+                            </ul>
+                        </div>
 
 
 
-            Insurance:
-            We participate with many major insurance carriers.
-
-            Private Health Plans:
-            HighMark
-            Capital Blue Cross
-            Aetna
-            Cigna
-            Coventry
-            Geisinger
-            Health Assurance
-            Humana
-            Keystone (Newborns only)
-            Meritain
-            PPHN
-            United
-            Tricare
 
 
-            Children's Health Insurance Plans (CHIP):
-            Highmark
-            Aetna Better Health
+                    </div>
+                    <div className="tile is-parent">
+                        <div className="tile is-child box">
+                            <p className="title">Insurance:</p>
+                            <p className="subtitle">We accept a wide range of insurance plans</p>
+                            <p>Private Health Plans:</p>
+                            <ul>
+                                <li>HighMark</li>
+                                <li>Capital Blue Cross</li>
+                                <li>Aetna</li>
+                                <li>Cigna</li>
+                                <li>Coventry</li>
+                                <li>Geisinger</li>
+                                <li>Health Assurance</li>
+                                <li>Humana</li>
+                                <li>Keystone (Newborns only)</li>
+                                <li>Meritain</li>
+                                <li>PPHN</li>
+                                <li>United</li>
+                                <li>Tricare</li>
+                            </ul>
+
+
+
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                {/* 
 
 
               Medical Assistance:
@@ -48,11 +82,11 @@ class About extends Component {
               About Us:
               This is the best doctor for kids!! All other doctors will kill your kids!!
              */}
-          
-        
-      </div>
-    );
-  }
+
+
+            </div>
+        );
+    }
 }
 
 export default About;
