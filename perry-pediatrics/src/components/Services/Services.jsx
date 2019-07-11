ce8f3206626982bb6b953cc8ed409def664c801c
Ryan Crawford <ryancrawfordny@gmail.com>
2019-07-11 14:19:05 -0400

    working on services page

--- perry-pediatrics/src/components/Services/Services.jsx
+++ perry-pediatrics/src/components/Services/Services.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import './Services.css';
 
 class Services extends Component {
     render() {
         return (
-            <div className="Services">
-                
+            <div className="services">
+                <h1 className="services-heading">
+                Services we provide
+                </h1>
             </div>
         );
     }
